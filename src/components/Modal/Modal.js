7766f518b34776c8f8cd31dfb59a558a1160cb3f
Anna Loiko <Anna_Loiko@epam.com>
2021-03-08 00:34:49 +0300

    Task4 - Add lifecycle methods

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,49 +1,62 @@
-import React, { useEffect } from "react";
+import React from "react";
 import ReactDOM from 'react-dom';
 import style from "./style.module";
 
 import PropTypes from 'prop-types';
 
-const Modal = (props) => {
-  const { isOpen, title, children, clickCloseModal } = props;
 
-  function escCloseModal(event) {
+class Modal extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isOpen: this.props.isOpen,
+      title: this.props.title,
+      children: this.props.children,
+    };
+    this.clickCloseModal = this.props.clickCloseModal.bind(this);
+    this.escCloseModal = this.escCloseModal.bind(this);
+  }
+
+  escCloseModal(event){
     if (event.keyCode === 27) {
-      clickCloseModal();
+      this.props.clickCloseModal();
     }
   }
 
-  useEffect(() => {
-    document.addEventListener("keydown", escCloseModal, false);
-    return () => {
-      document.removeEventListener("keydown", escCloseModal, true);
-    };
-  }, [])
+  componentDidMount(){
+    document.addEventListener("keydown", this.escCloseModal, false);
+  }
 
+  componentWillUnmount(){
+    document.removeEventListener("keydown", this.escCloseModal, false);
+  }
 
-  return ReactDOM.createPortal(
-    <>
-      {isOpen &&
-        <>
-          <div className={style.modalOverlay} onClick={(e) => {
-            e.stopPropagation();
-            clickCloseModal();
-          }}></div>
-          
-          <div className={style.modal}>
-            <div className={style.modalHeader}>
-              <span className={style.closeBtn} onClick={clickCloseModal}></span>
-              <h2>{title}</h2>
+  render(){
+    console.log(this.props);
+    return ReactDOM.createPortal(
+      <>
+        {this.props.isOpen &&
+          <>
+            <div className={style.modalOverlay} onClick={(e) => {
+              e.stopPropagation();
+              this.props.clickCloseModal();
+            }}></div>
+            
+            <div className={style.modal}>
+              <div className={style.modalHeader}>
+                <span className={style.closeBtn} onClick={this.props.clickCloseModal}></span>
+                <h2>{this.props.title}</h2>
+              </div>
+              <div className={style.modalContent}>
+                {this.props.children}
+              </div>
             </div>
-            <div className={style.modalContent}>
-              {children}
-            </div>
-          </div>
-        </>
-      }
-    </>,
-    document.getElementById("root")
-  );
+          </>
+        }
+      </>,
+      document.getElementById("root")
+    );
+  }
 }
 
 Modal.propTypes = {
